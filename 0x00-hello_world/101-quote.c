f1ec4755e8c75210480ee996f8b2a8d3744b34bf
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 13:45:45 -0700

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,7 @@
  * Return: 1 error
  */
 int main(void)
-{
-	char st[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char st[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(1, st, 57);
 	return (1);
 }
