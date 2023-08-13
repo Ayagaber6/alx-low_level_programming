44dea531650ce1d726b4fb497da0d1c0d3f29116
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 13:38:16 -0700

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- entery point
+ *
+ * Description: program print string without printf or puts functions
+ *
+ * Return: 1 error
+ */
+int main(void)
+{
+	char st=[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,st,57);
+	return (1);
+}
