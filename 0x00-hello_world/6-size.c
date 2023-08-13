3ab078fa7726658ad1ad222616effe975751e04f
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 12:51:07 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main- entery point
+ *
+ * Descritption: program that prints the size of various types on the computer
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("size of a char",sizeof(char));
+	printf("size of an int",sizeof(int));
+	printf("size of a long int",sizeof(long int));
+	printf("size of a long long int",sizeof(long long int));
+	printf"size of a float",sizeof(float));
+	return (0);
+}
