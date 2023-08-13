27ac49ab4b69c1cb2c6232b8f3ca66a7438f77f0
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 12:34:18 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Description: pring string using printf
+ *
+ * Return: 0 so program success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
