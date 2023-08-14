3ab587fee6626ba327b0749297b9629faf66d206
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 22:39:45 -0700

    l

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Description: reverse lowercase alphabet
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'z' ; x >= 'a' x--)
+	{
+		putchar(x);
+	}
+	putchar ('\n');
+	return (0);
+}
