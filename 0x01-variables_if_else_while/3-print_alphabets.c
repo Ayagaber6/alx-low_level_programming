a3e01a38ae82045f41a3830b37bda73156ac1a13
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 21:47:59 -0700

    l

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,11 +13,10 @@ int main(void)
 	for(x = 'a'; x <= 'z'; x++)
     {
         putchar(x);
-    }
-	putchar('\n');
 	for(y = 'A'; y <= 'Z'; y++)
     {
         putchar(y);
+    }
     }
 	putchar('\n');
 	return (0);
