83f90ea1b7f39c403a51cc01dc96186244dd0b24
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 22:16:10 -0700

    alphabet in lowercase except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int x;
 
-	for (x='a' ; x <= 'z' ; x++)
+	for (x = 'a' ; x <= 'z' ; x++)
 	{
 		if (x == 'q' || x == 'e')
 		{
 			continue;
 		}
 		putchar (x);
-		putchar ('\n');
 	}
+	putchar ('\n');
 	return (0);
 }
