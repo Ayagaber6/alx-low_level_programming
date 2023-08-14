79dd0dd0252af80b01bc59adb741b2a277548e64
Aya Gaber <ayagaber6@gmail.com>
2023-08-14 00:09:30 -0700

    l

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int x, y, z;
 
-	for (x = 0 ; x <= 9 ; x ++)
+	for (x = 0 ; x <= 9 ; x++)
 	{
-		for (y = 0 ; y <= 9 ; y ++)
+		for (y = 0 ; y <= 9 ; y++)
 		{
 			for (z = 0 ; z <= 9 ; z++)
 			{
-				if ( x != y && x != z && y != z )
+				if (x != y && x != z && y != z)
 				{
 					putchar(z);
 					putchar(y);
