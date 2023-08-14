3271a4326bc45a03c23ad83a81b0ae3c5894f851
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 23:03:20 -0700

    l

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Description: print all possible cominations of single num
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; i++)
+	{
+		putchar(n +", ");
+	}
+	putchar ('\n');
+	return (0)
+}
