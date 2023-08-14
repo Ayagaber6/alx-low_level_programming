02d8dd723e0be93f1a37f215c38193560e8c861a
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 19:54:08 -0700

    l

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/*
+ * main -entry point
+ *
+ * Description: Positive anything is better than negative nothing
+ *
+ * Return: 0 that is mean success
+ */
 int main(void)
 {
 	int n;
