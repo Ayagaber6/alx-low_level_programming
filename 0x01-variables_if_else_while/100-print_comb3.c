67860ac97dee84dc2c6afc0b7096d7192b57a431
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 23:40:14 -0700

    l

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,17 +14,17 @@ int main(void)
 	{
 		for (y = 0 ; y <= 9 ; y++)
 		{
-			if (x == y || y == x)
-			{
-				continue;
-			}
-			else
+			if (x != y && y != x)
 			{
 				putchar (x + '0');
 				putchar (y + '0');
 			}
 			putchar(',');
 			putchar(' ');
+			else
+			{
+				break;
+			}
 		}
 	}
 	return (0);
