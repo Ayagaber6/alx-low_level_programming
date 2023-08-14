faba2b0d6a5d074f03e5db72b22d5633d6654539
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 22:24:53 -0700

    l

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Description: program that prints all single digit numbers of base 10
+ *
+ * Return: 0 mean success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		if (i%2)
+		{
+			continue;
+		}
+		else 
+		{
+			printf("%d , i\n");
+		}
+	return (0);
+}
