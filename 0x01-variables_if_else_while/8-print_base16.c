ded6e818d80df7d6d651a6a42729450feece9d4f
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 22:44:58 -0700

    l

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Description: program that prints all the numbers of base 16 in lowercase
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n >= 16 ; n++)
+	{
+		putchar (n);
+	}
+	putchar('n');
+	return (0);
+}
