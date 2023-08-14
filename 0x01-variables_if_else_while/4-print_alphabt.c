2bc1310bdd8b1b7d8e5f78dd3dc0dc308b16dccc
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 22:11:13 -0700

    l

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Description: alphabet in lowercase
+ *
+ * Return: 0 that mean success
+ */
+int main(void)
+{
+	int x;
+
+	for (x='a' ; x <= 'z' ; x++)
+	{
+		if (x == 'q' || x == 'e')
+		{
+			continue;
+		}
+		putchar (x);
+		putchar ('\n');
+	}
+	return (0);
+}
