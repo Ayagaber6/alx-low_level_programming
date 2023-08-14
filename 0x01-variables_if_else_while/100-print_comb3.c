5456fdb8a4c368bb6e65435dd8994321c92b00a3
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 23:29:51 -0700

    l

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Description: different combinations of two digits
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int x,y;
+
+	for (x = 0 ; x <=9 ; x++)
+	{
+		for (y = 0 ; y <=9 ; y++)
+		{
+			if (x == y)
+			{
+				continue;
+			}
+			else
+			{
+				putchar (x);
+				putchar (y);
+		}
+	}
+	return (0);
+}
