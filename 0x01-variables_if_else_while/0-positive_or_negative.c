16bb9eb04927885af5e0acd29872764be8239119
Aya Gaber <ayagaber6@gmail.com>
2023-08-13 20:03:53 -0700

    l

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * main -entry point
+/**
+ * main- entry point
  *
  * Description: Positive anything is better than negative nothing
  *
