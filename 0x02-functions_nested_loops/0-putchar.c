77597b743ad08c5381ac1acc67d2c660a4458a35
Aya Gaber <ayagaber6@gmail.com>
2023-08-15 06:13:34 -0700

    print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Description: program that prints _putchar
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
