5dbab51d3d8fbd2ce0ce70c5046127e453cb93d4
Aya Gaber <ayagaber6@gmail.com>
2023-08-16 04:11:47 -0700

    l

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,19 @@
 #define MAIN_H
 
 #include <stdio.h>
+int main(void)
+{
+	int a, b, c;
+	int largest;
 
+	a = 972;
+	b = -98;
+	c = 0;
+
+	largest = largest_number(a, b, c);
+	printf("%d is the largest number\n", largest);
+
+	return (0);
+}
 void positive_or_negative(int i);
 #endif /* MAIN_H */
