3f8719a981462ad8976d54a3629c5905c8326064
Aya Gaber <ayagaber6@gmail.com>
2023-08-16 04:27:30 -0700

    l

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,11 +9,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && a >= c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b >= a && b >= c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
@@ -26,20 +26,16 @@ int largest_number(int a, int b, int c)
 }
 int main(void)
 {
+	int a, b, c;
 	int largest;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
+	a = 972;
+	b = -98;
+	c = 0;
 
-	return (largest);
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
+	return (0);
 }
