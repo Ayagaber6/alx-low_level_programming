e056f6cae86064514f5b55a134a5fbc7eb75a6f2
Aya Gaber <ayagaber6@gmail.com>
2023-08-16 18:12:37 -0700

    l

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,6 +5,20 @@
  * Description: find largest number
  * Return: largest number
  */
+int main(void)
+{
+	int a, b, c;
+	int largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+
+	largest = largest_number(a, b, c);
+	printf("%d is the largest number\n", largest);
+
+	return (0);
+}
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -23,19 +37,4 @@ int largest_number(int a, int b, int c)
 	}
 
 	return (largest);
-}
-int main(void)
-{
-	int a, b, c;
-	int largest;
-
-	a = 972;
-	b = -98;
-	c = 0;
-
-	largest = largest_number(a, b, c);
-
-	printf("%d is the largest number\n", largest);
-
-	return (0);
 }
