34480dc0b367cbc420f1859d61ed915f912fd098
Aya Gaber <ayagaber6@gmail.com>
2023-08-18 10:28:49 -0700

    l

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main- enrty point
+ *
+ * Description: print triangle with # character
+ * @size: input number
+ * Return: void
+ */
+void print_triangle/*print shape*/(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1 ; i <= size - 1 ; i++)
+		{
+			_putchar(' ');
+			for (j = 1 ; j <= size ; j++)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
