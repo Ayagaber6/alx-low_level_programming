19bfb25ffc2e2bcb5f0e6a2de60de0bb5212a704
Aya Gaber <ayagaber6@gmail.com>
2023-08-17 18:32:00 -0700

    l

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: 1 if digi else 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+}
