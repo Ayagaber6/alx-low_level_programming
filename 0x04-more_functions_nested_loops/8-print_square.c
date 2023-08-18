7b011592ab093638d7f38c4a20d2e06fb0ca96c5
Aya Gaber <ayagaber6@gmail.com>
2023-08-18 02:11:10 -0700

    l

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main- entry point
+ *
+ * Description: drwa square
+ * @size: input number
+ */
+void print_square/*draw square using '#'*/(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1 ; i <= size ; i++)
+		{
+			for (j = 1 ; j <= size ; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('#');
+		}
+		_putchar('\n);
+	}
+}
