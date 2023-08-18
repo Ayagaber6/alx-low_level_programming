c5fb90c7912ef4b576923c5422b3a6aa50205128
Aya Gaber <ayagaber6@gmail.com>
2023-08-18 01:27:45 -0700

    l

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,7 +16,8 @@ void more_numbers/*print numbers 10 times*/(void)
 		{
 			if (n > 9)
 			{
-				_putchar('1' + n % 10 + '0');
+				_putchar(1);
+				_putchar(n % 10 + '0');
 			}
 			_putchar(n + 48);
 		}
