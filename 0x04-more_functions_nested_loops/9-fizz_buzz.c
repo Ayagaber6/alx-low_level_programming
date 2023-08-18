46f16fec55806cd3d18e7cac1f6e137989e1eca4
Aya Gaber <ayagaber6@gmail.com>
2023-08-18 03:20:12 -0700

    l

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * main-enrty point
@@ -12,7 +11,8 @@ int fizz_buzz/*print fizz if multi 3 and buzz if multi 5 */(void)
 
 	for (n = 1 ; n <= 100 ; n++)
 	{
-		if (n % 3 == 0)&&(n % 5 == 0)
+		if
+			(n % 3 == 0)&&(n % 5 == 0)
 		{
 			printf("%s", "FizzBuzz");
 		}
