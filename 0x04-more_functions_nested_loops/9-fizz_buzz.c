890703ae799854b9eff840577740482d9f7ae184
Aya Gaber <ayagaber6@gmail.com>
2023-08-18 03:29:21 -0700

    l

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,8 +11,7 @@ int fizz_buzz/*print fizz if multi 3 and buzz if multi 5 */(void)
 
 	for (n = 1 ; n <= 100 ; n++)
 	{
-		if
-			(n % 3 == 0)&&(n % 5 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("%s", "FizzBuzz");
 		}
