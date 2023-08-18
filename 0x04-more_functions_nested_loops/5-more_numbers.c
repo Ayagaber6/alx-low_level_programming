c5a738d1ec06e7aa2f7c0ab484aaa02c732eecaf
Aya Gaber <ayagaber6@gmail.com>
2023-08-17 20:26:27 -0700

    l

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main- enrty point
+ *
+ * Description: print num from 0 to 14 - 10 times
+ *
+ * Return: always 0
+ */
+void more_numbers(void)
+{
+	int i, n;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (n = 0 ; i<=14 ; i++)
+		{
+			_putchar(n + 48);
+		}
+		_putchar(n + 48);
+	}
+	_putchar('\n');
+
+	return (0);
+}
