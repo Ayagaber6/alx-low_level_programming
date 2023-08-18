8f410377a5cc598ae5c533aa4e12959545403532
Aya Gaber <ayagaber6@gmail.com>
2023-08-18 01:07:04 -0700

    l

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,16 +3,18 @@
  * main- enrty point
  *
  * Description: print num from 0 to 14 - 10 times
+ * @i more_numbers: counter
+ * @n more_number: inputer number
  *
  * Return: always 0
  */
-void more_numbers(void)
+void more_numbers/*print numbers 10 times*/(void)
 {
 	int i, n;
 
 	for (i = 0 ; i <= 10 ; i++)
 	{
-		for (n = 0 ; i<=14 ; i++)
+		for (n = 0 ; i <= 14 ; i++)
 		{
 			_putchar(n + 48);
 		}
