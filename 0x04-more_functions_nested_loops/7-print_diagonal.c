e9a2a5be4bfc1a57538de441e0f7a7038b23818e
Aya Gaber <ayagaber6@gmail.com>
2023-08-18 16:02:23 -0700

    l

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,15 +18,10 @@ void print_diagonal/*draw diagonal*/(int n)
 		{
 			for (j = 0 ; j < n ; i++)
 			{
-				if (j == i)
-				{
-					_putchar('\\');
-				}
-				else if (j < i)
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
+			
 			}
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
