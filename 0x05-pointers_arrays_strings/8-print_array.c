bf0a2cc66be49f8ce34d1ed75940b468bc2da3ee
Aya Gaber <ayagaber6@gmail.com>
2023-08-22 16:05:11 -0700

    l

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,8 @@ void print_array/*print elements of an array*/(int *a, int n)
 
 	for (i = 0 ; i < (n - 1)  ; i++)
 	{
-		if ((a[i + 1]) != '\0')
-		{
-			printf("%d, ", a[i]);
-		}
-		else
+		printf("%d, ", a[i]);
+		if (i == (n - 1))
 		{
 			printf("%d", a[i]);
 		}
