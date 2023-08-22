afa82cc06c863fdee2fc098d6878fb860b591bbd
Aya Gaber <ayagaber6@gmail.com>
2023-08-22 08:31:59 -0700

    l

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,9 +15,9 @@ void print_rev/*print reverse striong*/(char *s)
                 len++;
         }
 
-	for (i = len ; i >= 0 i--)
+	for (i = len ; i = 0 i--)
 	{
-		_putchar(*s + 0);
+		_putchar(*s);
 	}
 	_putchar('\n');
 }
