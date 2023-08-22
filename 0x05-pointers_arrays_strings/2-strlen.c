d9e18579c7bc5b69f2ef996f974fc3f837686088
Aya Gaber <ayagaber6@gmail.com>
2023-08-22 07:32:52 -0700

    l

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main- entry-point
+ *
+ * Description: length of a string
+ * @s: input character
+ */
+int _strlen/* length of string*/(char *s)
+{
+	int length;
+
+	length = strlen(*s);
+}
