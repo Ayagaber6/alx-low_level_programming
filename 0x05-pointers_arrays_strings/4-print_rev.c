bc4f2ac092218b371878c29f84dd26d1a242ca31
Aya Gaber <ayagaber6@gmail.com>
2023-08-22 08:21:18 -0700

    l

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main- entry point
+ *
+ * Description: print reverse string
+ * @s: input character
+ * Return: string
+ */
+void print_rev/*print reverse striong*/(char *s)
+{
+	int i;
+
+	for (i = '\0' ; i >= 0 i--)
+	{
+		_putchar(*str + 0);
+	}
+	_putchar('\n');
+}
