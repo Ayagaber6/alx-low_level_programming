af30c6767c1dee1a383521ce2fac533563832fc2
Aya Gaber <ayagaber6@gmail.com>
2023-08-22 08:58:16 -0700

    l

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main- entry point
+ *
+ * Description: print reverse string
+ * @s: input character
+ */
+void rev_string(char *s)
+{
+	int i, len;
+
+	for (len = 0 ; *s != '\0' ; s++)
+	{
+		len++;
+	}
+
+	for (i = len ; i >= 0 ; i--)
+	{
+		_putchar(*s);
+	}
+}
