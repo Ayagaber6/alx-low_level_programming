e5f574c5a70270cb23885da19309856c62bdab5b
Aya Gaber <ayagaber6@gmail.com>
2023-08-22 07:51:44 -0700

    l

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main- entry point
+ *
+ * Description: print string
+ * @str: input character
+ * Return: 0 success
+ */
+void _puts/*print string*/(char *str)
+{
+	_putchar('%s');
+	_putchar('\n');
+}
