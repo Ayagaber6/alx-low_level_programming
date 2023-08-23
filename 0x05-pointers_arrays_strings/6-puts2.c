acec87724d51bea5eca67a6bac9ab747fe6a246e
Aya Gaber <ayagaber6@gmail.com>
2023-08-22 17:37:17 -0700

    l

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,9 @@ void puts2/*print even numb*/(char *str)
 {
 	int i;
 
-	for (i = 0 ; *str != '\0' ; i++)
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if (i == 0)
-		{
-			_putchar(str[0]);
-		}
-		else if (str[i] % 2 == 0)
+		if (str[i] % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
