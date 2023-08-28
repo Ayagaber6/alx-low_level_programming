6285a73efe450de880524cacc44005850f387aa1
Aya Gaber <ayagaber6@gmail.com>
2023-08-28 15:38:16 -0700

    l

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,14 +7,16 @@
  * @s: pointer
  * @b: input character
  * @n: input unsigned value
+ * Return: pointer to the memory area
  */
 char *_memset/* fill memory */(char *s, char b, unsigned int n)
 {
 	int i;
 
-	for (i = 0 ; n > 0 ; i++, n--)
+	for (i = 0 ; n > 0 ; i++)
 	{
 		s[i] = b;
+		n--;
 	}
 	return (s);
 }
