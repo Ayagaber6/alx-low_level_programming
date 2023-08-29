4887e9ec84520943ee1c89789bb9972a2a7a3e08
Aya Gaber <ayagaber6@gmail.com>
2023-08-29 15:46:54 -0700

    l

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,10 +13,9 @@ char *_memset/* fill memory */(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0 ; i < n ; i++)
+	for (i = 0 ; i < n ; i++, n--)
 	{
 		s[i] = b;
-		n--;
 	}
 	return (s);
 }
