1ac20f33b20d35ffe844bac87ba365447c7bcb3c
Aya Gaber <ayagaber6@gmail.com>
2023-08-30 16:41:08 -0700

    l

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,14 +8,14 @@
  */
 int is_prime_number/*check number*/(int n)
 {
-	return (check(n, 2));
+	return (check_num(n, 2));
 }
-int check(int n, int i)
+int check_num(int n, int i)
 {
 	if (i >= n && n > 1)
 		return (1);
 	else if (n % i == 0 || n <= 1)
 		return (0);
 	else
-		return (check(n, i + 1));
+		return (check_num(n, i + 1));
 }
