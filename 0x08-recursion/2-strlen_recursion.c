a41f282fe814ff1a88b5b401f817c13f11cc8cb2
Aya Gaber <ayagaber6@gmail.com>
2023-08-30 14:23:49 -0700

    l

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main- entry point
+ *
+ * Description: return string length
+ * @s: string pointer
+ */
+int _strlen_recursion/*return string length*/(char *s)
+{
+	return (strlen(s));
+}
