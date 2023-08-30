9bce4940d6e8f3a5c5d036eb2edf029eb8135ae7
Aya Gaber <ayagaber6@gmail.com>
2023-08-30 14:32:09 -0700

    l

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main- entry point
+ *
+ * Description: return factorial of an number
+ * @n: input number
+ * Return: fact of num
+ */
+int factorial/*fact*/(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+		return (n * fact(n - 1));
+}
