50b6e059347930f82a9b53c826c69b8d3a4ad3db
Aya Gaber <ayagaber6@gmail.com>
2023-08-30 13:41:09 -0700

    l

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,9 @@ void _puts_recursion/*print string*/(char *s)
 {
 	if  (*s != '\0')
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar(s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
