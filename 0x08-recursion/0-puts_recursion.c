d8e772c111c856a5fb7adf5e4b580afe2f21489f
Aya Gaber <ayagaber6@gmail.com>
2023-08-30 13:11:29 -0700

    l

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main- entry point
+ *
+ * Description: print string
+ * @s: string pointer
+ */
+void _puts_recursion/*print string*/(char *s)
+{
+	if  (*s != '\0')
+	{
+		_puts_recursion(s);
+	}
+}
