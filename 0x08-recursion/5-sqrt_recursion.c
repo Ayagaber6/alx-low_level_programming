8ef60947e77afec89805319e04a9b60d3a0b95c6
Aya Gaber <ayagaber6@gmail.com>
2023-08-30 15:39:57 -0700

    l

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main- entry point
+ * Description: return the natural square root of a num
+ * @n: input value
+ * Return: square root of num
+ */
+int _sqrt_recursion/*square root*/(int n)
+{
+	if (_sqrt_recursion == double)
+		return (-1);
+	else
+		return (_sqrt_recursion(n));
+}
