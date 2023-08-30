7a0154a5ddd21c74a115e212125fd0337fe65da9
Aya Gaber <ayagaber6@gmail.com>
2023-08-30 13:54:48 -0700

    l

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,10 +5,10 @@
  * Description: print string in reverse
  * @s: string pointer
  */
-void _print_rev_recursion(char *s)
+void _print_rev_recursion/*print reverse string*/(char *s)
 {
 	if (*s != '\0')
-	}
+	{
 		*s++;
 	}
 	else
