d4869377f61f8ed062a3a5d7b3de38ead82e626a
Aya Gaber <ayagaber6@gmail.com>
2023-08-30 18:12:00 -0700

    l

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * wildcmp- compares two strings
- * @s: string pointer
+ * @s1: string pointer
+ * @s2: string pointer
  * Return: 0 or 1
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s == '\0')
+	if (*s1 == '\0' || *s2 == '\0')
 	{
 		return (1);
 	}
