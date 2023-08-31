66e738e3d7d12ba087557858c6f3dc7694631142
Aya Gaber <ayagaber6@gmail.com>
2023-08-30 17:53:19 -0700

    l

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,14 +5,15 @@
  * Return: 0 or 1
  */
 int is_palindrome(char *s)
-{       
-        int i = 0;
-        int n = strlen(*s);
+{
+	int i = 0;
+	int n = strlen(*s);
 
-        if (*s[i] == *s[n-1])
-        {
-                is_palindrome(i + 1, n - 1);
-                return (1);
-        }
-        else
-                return (0);
+	if (*s[i] == *s[n - 1])
+	{
+		is_palindrome(i + 1, n - 1);
+		return (1);
+	}
+	else
+		return (0);
+}
