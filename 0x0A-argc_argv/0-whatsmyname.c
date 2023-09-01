17a63429f315263e71d29dd49a821388bf22959d
Aya Gaber <ayagaber6@gmail.com>
2023-09-01 16:07:52 -0700

    l

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _myname- print its name
+ * @argc: input value
+ * @argv: input array
+ * Return: 0 success
+ */
+int _myname(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
