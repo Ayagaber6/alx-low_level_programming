d57256de4736d0d04b35fd1d9e8040fa3e1892e5
Aya Gaber <ayagaber6@gmail.com>
2023-09-01 16:23:40 -0700

    l

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- print the number of arguments
+ * @argc: input value
+ * @argv: input array
+ * Return: 0 success
+ */
+int main(int argc, char *argv)
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
