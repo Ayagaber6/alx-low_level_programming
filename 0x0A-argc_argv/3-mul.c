1b89790b23d71d79dfa6d64adf9ada035fb61bd4
Aya Gaber <ayagaber6@gmail.com>
2023-09-01 17:16:21 -0700

    l

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,18 +5,17 @@
  * @argv: input array
  * Return 0 or 1
  */
-int main(int argc, int argv[])
+int main(int argc, char *argv[])
 {
+	int a;
+	int b;
+	
+	a = argv[1];
+	b = argv[2];
 	if (argc != 3)
 	{
 		printf("Error");
 		return (1);
 	}
-	int i;
-
-	for (i = 0; i <= argc ; i++)
-	{
 	printf("%d\n", argv[1] * argv[2]);
-	}
-	return (0);
 }
