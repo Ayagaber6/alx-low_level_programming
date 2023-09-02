f54ca3923f651db633c9914650ee9d11356ee9e4
Aya Gaber <ayagaber6@gmail.com>
2023-09-01 17:30:24 -0700

    l

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,18 +7,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int a;
-	int b;
-	
-	a = argv[1];
-	b = argv[2];
-	if (argc == 3)
+	int i;
+
+	for (i = 0 ; i <= argc ; i++)
 	{
-		printf("%d\n", a * b);
-	}
-	else
-	{
-		printf("Error");
-		return (1);
+		if (argc == 3)
+		{
+			printf("%d\n", argv[1] * argv[2]);
+		}
+		else
+		{
+			printf("Error");
+			return (1);
+		}
 	}
 }
