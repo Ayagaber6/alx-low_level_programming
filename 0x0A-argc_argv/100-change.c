16843b2c8d84b782839692c0a73999eb50129620
Aya Gaber <ayagaber6@gmail.com>
2023-09-01 20:21:42 -0700

    l

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- prints the minimum number of coins
+ * @argc: input value
+ * @argv: input array
+ *
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv)
+{
+	int cents;
+	int coins = 0;
+
+}
